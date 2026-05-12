0354af08cdbc65b6be82992360a175bbf397f505
Alvin Bryant <alvinjbryant@gmail.com>
2026-05-11 21:19:40 -0500

    Create Deck.h

---
+++ Deck.h
@@ -0,0 +1,41 @@
+//preprocessor guards
+#ifndef DECK_H
+#define DECK_H
+
+
+
+#define DECK_SIZE 52
+
+
+typedef enum {
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES
+} Suit;
+
+typedef enum {
+    TWO = 2, THREE, FOUR, FIVE,
+    SIX, SEVEN, EIGHT, NINE, TEN,
+    JACK, QUEEN, KING, ACE
+} Rank;
+
+typedef struct {
+    Suit suit;
+    Rank rank;
+} Card;
+
+typedef struct {
+    Card cards[DECK_SIZE];
+    int top;
+} Deck;
+
+void initDeck(Deck *deck);
+void shuffleDeck(Deck *deck);
+Card drawCard(Deck *deck);
+
+
+
+
+
+#endif
