d04664961142e89cf9bd59acaa62ca770ffd9de8
Alvin Bryant <alvinjbryant@gmail.com>
2026-05-14 23:03:34 -0500

    Create Menu.c
    Basic menu function

---
+++ Menu.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Menu.h"
+
+
+
+void printMenu() {
+    
+
+    printf("=================================\n");
+    printf("        CARD GAME MENU           \n");
+    printf("=================================\n\n");
+
+    printf("1. VC Mode\n");
+    printf("2. Texas Hold'em\n");
+    printf("3. Blackjack\n");
+    printf("4. Old Maid\n");
+    printf("5. Exit\n\n");
+
+    printf("Select option: ");
+}
